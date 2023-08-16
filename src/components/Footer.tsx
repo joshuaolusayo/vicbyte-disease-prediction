fb3795e12d354426ca83d21e27af719cbdb55768
Joshua Oyeleke <oyelekeoluwasayo@gmail.com>
2023-08-16 22:06:09 +0100

    changed the year format in the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,9 @@ const Footer = () => {
   return (
     <div className="bg-black px-6 py-20 md:py-24 lg:py-32 grid gap-y-8 text-gray-200">
       <div className="container mx-auto">
-        <p className="text-2xl text-center mb-0">&copy; MedPest 2023</p>
+        <p className="text-2xl text-center mb-0">
+          &copy; MedPest {new Date().getFullYear()}
+        </p>
       </div>
     </div>
   );
